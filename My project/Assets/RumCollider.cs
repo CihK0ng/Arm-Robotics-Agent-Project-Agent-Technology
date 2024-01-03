417015b28893b6f289f74e4330d8bb35018614b6
Chử Quốc Chính <gunnymagi@gmail.com>
2024-01-03 21:18:54 +0700

    commit

---
+++ My project/Assets/RumCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HandRobot"))
+        {
+            // Chai nước chạm vào tay robot, thực hiện các hành động cần thiết
+            Debug.Log("Robot hand touched the water bottle.");
+            // Lấy thông tin cần thiết từ chai nước (ví dụ: mức nước, loại nước, ...)
+        }
+    }
+
+}
