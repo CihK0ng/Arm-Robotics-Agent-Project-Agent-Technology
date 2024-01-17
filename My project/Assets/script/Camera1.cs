f262e306ad7c909672316ec63ab31473ca2d34d3
Chử Quốc Chính <gunnymagi@gmail.com>
2024-01-17 21:43:04 +0700

    add commit

---
+++ My project/Assets/script/Camera1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera1 : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    [SerializeField] private GameObject camera1;
+    // Start is called before the first frame update
+   
+    
+
+    void Update()
+    {
+        // Lấy giá trị đầu vào từ phím D
+        float zInput = Input.GetKey(KeyCode.D) ? 1 : 0;
+        // Di chuyển camera theo chiều Z
+        Vector3 newPosition = camera1.transform.position + new Vector3(0, 0, zInput * moveSpeed * Time.deltaTime);
+        camera1.transform.position = newPosition;
+
+        float zInputa = Input.GetKey(KeyCode.A) ? 1 : 0;
+        // Di chuyển camera theo chiều Z
+        Vector3 newPositiona = camera1.transform.position - new Vector3(0, 0, zInputa * moveSpeed * Time.deltaTime);
+        camera1.transform.position = newPositiona;
+
+
+    }
+}
